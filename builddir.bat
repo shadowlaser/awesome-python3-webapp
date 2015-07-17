17bb1739a5d1b580c8b0c4a05e59a1922ee93f29
shadowlaser <yang3911@126.com>
2015-07-17 16:08:04 +0800

    day 1:init directorys

---
+++ builddir.bat
@@ -0,0 +1,39 @@
+@echo off
+mkdir backup
+cd backup
+echo * > .gitignore
+cd ..
+mkdir conf
+cd conf
+echo * > .gitignore
+cd ..
+mkdir dist
+cd dist
+echo * > .gitignore
+cd ..
+mkdir www
+cd www
+echo * > .gitignore
+cd ..
+mkdir ios
+cd ios
+echo * > .gitignore
+cd ..
+mkdir android
+cd android
+echo * > .gitignore
+cd ..
+mkdir LICENSE
+cd LICENSE
+echo * > .gitignore
+cd ..
+cd www
+echo * > .gitignore
+mkdir static
+cd static
+echo * > .gitignore
+cd ..
+mkdir templates
+cd templates
+echo * > .gitignore
+cd ..
